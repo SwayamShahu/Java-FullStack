d114dd5fbfde7ef52544522aeaec1d141b324a47
Swayam <swayamshahu134@gmail.com>
2025-12-15 10:45:09 +0530

    Day 4

--- Day4/QuestionMain.java
+++ Day4/QuestionMain.java
@@ -2,7 +2,12 @@ package Day4;
 
 public class QuestionMain {
     static void main(String[] args) {
-        Question q = new Question();
-        q.removeLeadingZero();
+//        Question q = new Question();
+//        q.removeLeadingZero();
+        try{
+            System.out.println(10/0);
+        }catch (Exception e){
+            System.out.println(e.fillInStackTrace());
+        }
     }
 }
