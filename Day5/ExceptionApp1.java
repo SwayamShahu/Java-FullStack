c721e12f2d8e1fcec47d6b9f0fd4a6a5ed658408
Swayam <swayamshahu134@gmail.com>
2025-12-15 13:27:41 +0530

    Day 5

---
+++ Day5/ExceptionApp1.java
@@ -0,0 +1,57 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class ExceptionApp1 {
+    static int divide(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st Number: ");
+        int a = sc.nextInt();
+        System.out.println();
+        System.out.print("Enter 2st Number: ");
+        int b = sc.nextInt();
+        System.out.println();
+        int c = -1;
+        try {
+            c = a/b;
+            System.out.println("On dividing: " + c);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return c;
+    }
+    static void main(String[] args) {
+//        int[] arr = new int[10];
+//        try{
+////            int a = 10;
+////            int b = 0;
+////            int c = a/b;
+//            String str = null;
+//            System.out.println(str.length());
+//
+//
+//        } catch (Exception e) {
+////            System.out.println("Arithmetic Exception Occurs: Dividing by Zero !!");
+//            System.out.print("Exception Occur: ");
+//            System.out.println(e);
+//        }
+
+//        try {
+//            int a = 10;
+//            int b = 0;
+//            System.out.println(a/b);
+//            System.out.println("Try End !!!");
+//
+//        }catch (NullPointerException e){
+//            System.out.println(e);
+//        } catch (ArithmeticException e){
+//            System.out.println(e);
+//        }catch (ArrayIndexOutOfBoundsException e){
+//            System.out.println(e);
+//        }finally {
+//            System.out.println("Finally block running here !!!!!!!");
+//        }
+
+        divide();
+    }
+}
